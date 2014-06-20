f273df1d2d303a74fdb3409c247879c3cb7ab0d7
bagel <freetgm@gmail.com>
2014-06-20 21:41:23 +0800

    update server

--- server/server.py
+++ server/server.py
@@ -38,31 +38,65 @@ def request_sock(request_data):
         data[1] = '/'.join(data_url)
     elif data[0] == 'CONNECT':
         host = data[1].split(':')[0]
-        port = 443   
+        port = 443
+        request_data = '\r\n'.join(request_data)
+        s.connect((host, port))
+        s.send(request_data)
+        fp = s.makefile()
+        response_data = ""
+        response_data = fp.read(1024)
+        print response_data
+        return response_data
     data = ' '.join(data)
     request_data = request_data[1:]
     request_data.insert(0, data)
-    for i in xrange(len(request_data)):
-        if re.match('Accept-Encoding', request_data[i]):
-            request_data.pop(i)
-            break
     request_data = '\r\n'.join(request_data)
     print request_data
     s.connect((host, port))
     s.send(request_data)
     fp = s.makefile()
     response_data = ""
+    length = None
+    chunked = None
     line = fp.readline()
     while line:
         print [line]
         if line == '\r\n':
             break
         if re.match('Content-Length', line):
-            length = line.split(': ')[1].strip()
+            length = int(line.split(': ')[1].strip())
+        elif re.match('Transfer-Encoding', line):
+            chunked = 1
         response_data += line
         line = fp.readline()
     response_data += '\r\n'
-    response_data += fp.read(int(length))
+    if not length and chunked:
+        while True:
+            line = fp.readline()
+            response_data += line
+            length = int(line, 16)
+            if length == 0:
+                break
+            response_data += fp.read(length)
+            response_data += fp.read(2)
+            length = None
+        while True:
+            line = fp.readline()
+            if not line:
+                break
+            response_data += line
+            if line == '\r\n':
+                break
+    elif length and not chunked:
+        response_data += fp.read(length)
+    elif not length and not chunked:
+        while True:
+            line = fp.readline()
+            if not line:
+                break
+            response_data += line
+            if line == "\r\n":
+                break
     #print response_data
     return response_data
 
@@ -80,7 +114,9 @@ def start_server():
                 if len(recv_data) < 10240:
                     break
             request_data = base64.b64decode(request_data)
-            print request_data
+            print "request: ", request_data
+            if not request_data:
+                continue
             response_data = request_sock(request_data)
             #print response_data
             conn.send(base64.b64encode(response_data))
